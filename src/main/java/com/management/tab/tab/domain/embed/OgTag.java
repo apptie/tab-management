241d80ae802f6f057dd21e3e3058821512648d2b
apptie <appti@naver.com>
2024-03-29 22:28:40 +0900

    feat: OgTag changeContent() 호출 시 검증 메서드 추가

--- src/main/java/com/management/tab/tab/domain/embed/OgTag.java
+++ src/main/java/com/management/tab/tab/domain/embed/OgTag.java
@@ -1,5 +1,6 @@
 package com.management.tab.tab.domain.embed;
 
+import com.management.tab.tab.domain.exception.InvalidOgTagContentException;
 import lombok.Getter;
 
 @Getter
@@ -7,19 +8,39 @@ public class OgTag {
 
     private static final String EMPTY = "";
 
-    public static final OgTag DEFAULT_OG_TAG = new OgTag(EMPTY, EMPTY, EMPTY);
+    public static final OgTag DEFAULT = new OgTag(EMPTY, EMPTY, EMPTY);
 
     private final String imageUrl;
     private final String title;
     private final String description;
 
-    public OgTag(String imageUrl, String title, String description) {
+    private OgTag(String imageUrl, String title, String description) {
         this.imageUrl = imageUrl;
         this.title = title;
         this.description = description;
     }
 
     public OgTag changeContent(String imageUrl, String title, String description) {
+        validateContent(imageUrl, title, description);
+
         return new OgTag(imageUrl, title, description);
+    }
+
+    private void validateContent(String imageUrl, String title, String description) {
+        if (isInvalidImageUrl(imageUrl) || isInvalidTitle(title) || isInvalidDescription(description)) {
+            throw new InvalidOgTagContentException();
+        }
+    }
+
+    private boolean isInvalidImageUrl(String imageUrl) {
+        return imageUrl == null || imageUrl.isBlank();
+    }
+
+    private boolean isInvalidTitle(String title) {
+        return title == null || title.isBlank();
+    }
+
+    private boolean isInvalidDescription(String description) {
+        return description == null || description.isBlank();
     }
 }
--- src/test/java/com/management/tab/tab/domain/embed/OgTagTest.java
+++ src/test/java/com/management/tab/tab/domain/embed/OgTagTest.java
@@ -1,19 +1,23 @@
 package com.management.tab.tab.domain.embed;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.management.tab.tab.domain.exception.InvalidOgTagContentException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class OgTagTest {
 
     @Test
-    void changeContent_메서드는_전달한_파라미터로_값을_변경한다() {
+    void changeContent_메서드는_유효한_파라미터를_전달하면_전달한_값으로_초기화한_OgTag를_변경한다() {
         // given
-        OgTag ogTag = new OgTag("imageUrl", "title", "description");
+        OgTag ogTag = OgTag.DEFAULT;
 
         // when
         String expectedImageUrl = "changedImageUrl";
@@ -26,5 +30,38 @@ class OgTagTest {
         assertThat(actual.getImageUrl()).isEqualTo(expectedImageUrl);
         assertThat(actual.getTitle()).isEqualTo(expectedTitle);
         assertThat(actual.getDescription()).isEqualTo(expectedDescription);
+    }
+
+    @ParameterizedTest(name = "imageUrl이 {0}일 때 예외가 발생한다")
+    @NullAndEmptySource
+    void changeContent_메서드는_유효하지_않은_imageUrl을_전달하면_예외가_발생한다(String invalidImageUrl) {
+        // given
+        OgTag ogTag = OgTag.DEFAULT;
+
+        // when & then
+        assertThatThrownBy(() -> ogTag.changeContent(invalidImageUrl, "제목", "설명"))
+                .isInstanceOf(InvalidOgTagContentException.class);
+    }
+
+    @ParameterizedTest(name = "title이 {0}일 때 예외가 발생한다")
+    @NullAndEmptySource
+    void changeContent_메서드는_유효하지_않은_title을_전달하면_예외가_발생한다(String invalidTitle) {
+        // given
+        OgTag ogTag = OgTag.DEFAULT;
+
+        // when & then
+        assertThatThrownBy(() -> ogTag.changeContent("이미지 링크", invalidTitle, "설명"))
+                .isInstanceOf(InvalidOgTagContentException.class);
+    }
+
+    @ParameterizedTest(name = "description이 {0}일 때 예외가 발생한다")
+    @NullAndEmptySource
+    void changeContent_메서드는_유효하지_않은_description을_전달하면_예외가_발생한다(String invalidDescription) {
+        // given
+        OgTag ogTag = OgTag.DEFAULT;
+
+        // when & then
+        assertThatThrownBy(() -> ogTag.changeContent("이미지 링크", "제목", invalidDescription))
+                .isInstanceOf(InvalidOgTagContentException.class);
     }
 }
