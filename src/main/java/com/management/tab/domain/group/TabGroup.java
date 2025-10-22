8e67a4053f8880dbc6070337fc13e8df585a9b7b
apptie <appti@naver.com>
2025-10-22 20:55:58 +0900

    feat: TabGroup 시간 관련 필드 추가

--- src/main/java/com/management/tab/domain/group/TabGroup.java
+++ src/main/java/com/management/tab/domain/group/TabGroup.java
@@ -1,5 +1,6 @@
 package com.management.tab.domain.group;
 
+import com.management.tab.domain.common.AuditTimestamps;
 import com.management.tab.domain.group.vo.TabGroupId;
 import com.management.tab.domain.group.vo.TabGroupName;
 import lombok.Getter;
@@ -9,21 +10,23 @@ public class TabGroup {
 
     private final TabGroupId id;
     private final TabGroupName name;
+    private final AuditTimestamps timestamps;
 
     public static TabGroup create(String name) {
-        return new TabGroup(null, TabGroupName.create(name));
+        return new TabGroup(null, TabGroupName.create(name), AuditTimestamps.now());
     }
 
     public static TabGroup createWithAssignedId(Long id, TabGroup tabGroup) {
-        return new TabGroup(TabGroupId.create(id), tabGroup.name);
+        return new TabGroup(TabGroupId.create(id), tabGroup.name, tabGroup.timestamps);
+    }
+
+    private TabGroup(TabGroupId id, TabGroupName name, AuditTimestamps timestamps) {
+        this.id = id;
+        this.name = name;
+        this.timestamps = timestamps;
     }
 
     public TabGroup rename(String newName) {
-        return new TabGroup(this.id, TabGroupName.create(newName));
-    }
-
-    private TabGroup(TabGroupId id, TabGroupName name) {
-        this.id = id;
-        this.name = name;
+        return new TabGroup(this.id, TabGroupName.create(newName), this.timestamps);
     }
 }
