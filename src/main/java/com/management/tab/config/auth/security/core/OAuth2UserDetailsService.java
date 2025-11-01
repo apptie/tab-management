48598f61efe5cbbcb709bfc89310869f5151196f
apptie <appti@naver.com>
2025-11-01 19:56:21 +0900

    feat: 인증 토큰, 사용자 정보, 사용자 조회 서비스 구현

---
+++ src/main/java/com/management/tab/config/auth/security/core/OAuth2UserDetailsService.java
@@ -0,0 +1,30 @@
+package com.management.tab.config.auth.security.core;
+
+import com.management.tab.config.auth.security.PrivateClaims;
+import com.management.tab.config.auth.security.TokenDecoder;
+import com.management.tab.config.auth.security.enums.TokenType;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class OAuth2UserDetailsService implements UserDetailsService {
+
+    private final TokenDecoder tokenDecoder;
+
+    @Override
+    public OAuth2UserDetails loadUserByUsername(String token) throws UsernameNotFoundException {
+        return tokenDecoder.decode(TokenType.ACCESS, token)
+                           .map(this::convert)
+                           .orElse(null);
+    }
+
+    private OAuth2UserDetails convert(PrivateClaims privateClaims) {
+        return new OAuth2UserDetails(
+                privateClaims.accountId(),
+                Set.of(new SimpleGrantedAuthority(privateClaims.roleName()))
+        );
+    }
+}
